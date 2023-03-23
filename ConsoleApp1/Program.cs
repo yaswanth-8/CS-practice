b3687787079809fd3150ecd4e232b010b21e9524
yaswanth <ya8wanth@gmail.com>
2023-03-23 11:37:26 +0530

    Working on constructor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,19 +5,29 @@ namespace Training
 {
     class Program
     {
+        string Name;
+        int id;
+        string department;
+        float grade;
+        public Program(string myName,int myId,string myDepartment,float myGrade) {
+           this.Name = myName;
+            this.id = myId;
+            this.department = myDepartment;
+            this.grade = myGrade;
+        }
+        public void displayStudent()
+        {
+            Console.WriteLine("Details of the student are : " + Name+ " is th name ");
+            Console.WriteLine(id + " is id " + department + "is dept " + grade + " is grade");
+        }
        
         static void Main(string[] args)
         {
-            int[][] arr = new int[2][];
-            arr[0] = new int[] { 1, 2 };
-            Console.WriteLine(arr[0].Length);
-           /* for(int i = 0; i < 2; i++)
-            {
-                for(int j = 0; j <3; j++)
-                {
-                    Console.WriteLine(arr[i, j]);
-                }
-            }*/
+
+            Program myProgram = new Program("yaswanth",8,"automobile",8.8f);
+            myProgram.displayStudent();
+            Program myProgram1 = new Program("varun", 10, "cs", 9.2f);
+            myProgram1.displayStudent();
         }
     
     }
